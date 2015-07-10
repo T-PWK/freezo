4a352998446b8e272420b2182c45dd6e7c17060e
tomasz.pawlak <tomasz.pawlak@thebodyshop.com>
2015-07-10 20:01:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Freezo #
+
+Freezo is a free, open source, Content Management System built on the Spring Framework platform.
---
+++ src/main/java/org/freezo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.freezo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer
+{
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application)
+	{
+		return application.sources(Application.class);
+	}
+}
