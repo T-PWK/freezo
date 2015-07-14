11d0fad73376ac0b71b3cee723044e63605a0f60
T-PWK <tompwk@gmail.com>
2015-07-14 07:07:08 +0100

    Authentication action

--- src/main/java/org/freezo/admin/config/AdminConfiguration.java
+++ src/main/java/org/freezo/admin/config/AdminConfiguration.java
@@ -4,17 +4,26 @@ import org.freezo.admin.service.AdminUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableWebSecurity
 @Configuration
-@ConditionalOnProperty(prefix = "freezo", name = "enableAdmin", havingValue = "true")
+@Profile("admin")
 public class AdminConfiguration
 {
 	@Bean
@@ -33,7 +42,32 @@ public class AdminConfiguration
 	}
 
 	@Configuration
-	@ConditionalOnProperty(prefix = "freezo", name = "enableAdmin", havingValue = "true")
+	protected static class ViewsConfiguration extends WebMvcConfigurerAdapter
+	{
+		@Override
+		public void addViewControllers(final ViewControllerRegistry registry)
+		{
+			registry.addViewController("/admin/login").setViewName("admin/login");
+			registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		}
+	}
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	@Profile("admin")
+	protected static class SecurityConfig extends WebSecurityConfigurerAdapter
+	{
+		@Override
+		protected void configure(final HttpSecurity http) throws Exception
+		{
+			http.antMatcher("/admin/**").authorizeRequests().anyRequest().authenticated()
+					.and().formLogin().loginPage("/admin/login").defaultSuccessUrl("/admin/cms", true).permitAll()
+					.and().logout().logoutUrl("/admin/logout").permitAll();
+		}
+	}
+
+	@Configuration
+	@Profile("admin")
 	protected static class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter
 	{
 		@Autowired
@@ -45,7 +79,8 @@ public class AdminConfiguration
 		@Override
 		public void init(final AuthenticationManagerBuilder auth) throws Exception
 		{
-			auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+//			auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+			auth.userDetailsService(userDetailsService);
 		}
 	}
 }
