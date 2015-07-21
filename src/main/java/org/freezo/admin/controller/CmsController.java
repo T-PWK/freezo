40bee7cc5cf6786958ec2b9be60417b160775042
T-PWK <tompwk@gmail.com>
2015-07-22 00:02:22 +0100

    Partial pages

--- src/main/java/org/freezo/admin/controller/CmsController.java
+++ src/main/java/org/freezo/admin/controller/CmsController.java
@@ -2,6 +2,7 @@ package org.freezo.admin.controller;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +14,11 @@ public class CmsController
 	public String cms()
 	{
 		return "admin/cms";
+	}
+
+	@RequestMapping("partial/{name}")
+	public String partial(@PathVariable("name") final String name)
+	{
+		return "admin/partial/" + name;
 	}
 }
