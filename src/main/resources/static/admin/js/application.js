1abbcea1735cf580c323dc22831718fed03bc2e5
T-PWK <tompwk@gmail.com>
2015-07-22 00:03:08 +0100

    AngularJS application

--- src/main/resources/static/admin/js/application.js
+++ src/main/resources/static/admin/js/application.js
@@ -1,5 +1,17 @@
-/* global angular */
-angular.module('freezo', [])
-	.controller('HelloCtrl', ['$scope', function ($scope) {
-		$scope.info = "Hello from Angular in JS";
-	}]);
+/// <reference path="../../../../../../typings/angularjs/angular.d.ts"/>
+(function () {
+	angular.module('tbs', ['ngRoute'])
+		.config(['$routeProvider', function ($routeProvider) {
+			$routeProvider
+				.when('/dashboard', { templateUrl: '/admin/partial/dashboard' })
+				.when('/user/profile', { templateUrl: '/admin/partial/user-profile' })
+				.when('/user/settings', { templateUrl: '/admin/partial/user-settings' })
+				.when('/admin/users', { templateUrl: '/admin/partial/users' })
+				.when('/admin/themes', { templateUrl: '/admin/partial/themes' })
+				.when('/admin/plugins', { templateUrl: '/admin/partial/plugins' })
+				.otherwise('/dashboard');
+		}])
+		.controller('DashboardCtrl', ['$scope', function ($scope) {
+			$scope.data = 'Hi';
+		}])
+} ());
