5fc3a529ca90fa99269804782ec53860ea72fdc9
T-PWK <tompwk@gmail.com>
2015-07-22 22:37:24 +0100

    Admin console updates

--- src/main/resources/static/admin/js/application.js
+++ src/main/resources/static/admin/js/application.js
@@ -20,26 +20,40 @@
 		}])
 		.controller('UsersCtrl', ['$scope', 'User', function ($scope, User) {
 			$('.main .dropdown').dropdown();
+
+			$scope.filter = null;
+			$scope.sort = null;
+			$scope.asc = true;
+			$scope.pageNumber = 0;
+
 			$scope.query = { filter: null, sort: null, page: null };
 			$scope.filterBy = function (filter) {
-				$scope.query.filter = filter;
+				$scope.filter = filter;
 			};
 			$scope.sortBy = function (sort) {
-				$scope.query.sort = sort;
+				if ($scope.sort == sort) {
+					$scope.asc = !$scope.asc;
+				} else {
+					$scope.sort = sort;
+					$scope.asc = true;
+				}
 			};
 			$scope.page = function (page) {
-				$scope.query.page = page < 2 ? null : page - 1;
+				$scope.pageNumber = page < 2 ? null : page - 1;
 			};
 			$scope.reload = function () {
-				User.get($scope.query, function (details) {
-					$scope.details = details;
-					$scope.users = details.content;
+				User.get({ 
+					sort: $scope.sort ? $scope.sort + ',' + ($scope.asc ? 'asc' : 'desc') : null, 
+					page: $scope.pageNumber, 
+					filter: $scope.filter }, function (details) {
+						$scope.details = details;
+						$scope.users = details.content;
 				});
 			};
 			$scope.info = function (user) {
 				$scope.user = user;
 				$('.ui.modal').modal('show');
 			}
-			$scope.$watchGroup(['query.filter', 'query.sort', 'query.page'], $scope.reload);
+			$scope.$watchGroup(['filter', 'sort', 'asc', 'pageNumber'], $scope.reload);
 		}])
 } ());
