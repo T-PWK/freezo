7ae50794a91f8f0f1f6bc5bde3ab9766c03d068d
T-PWK <tompwk@gmail.com>
2015-07-22 16:55:53 +0100

    Users API controler updates
    Selection of users by type

--- src/main/java/org/freezo/admin/controller/UserController.java
+++ src/main/java/org/freezo/admin/controller/UserController.java
@@ -1,11 +1,53 @@
 package org.freezo.admin.controller;
 
+import org.freezo.domain.User;
+import org.freezo.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/users")
 @Profile("admin")
 public class UserController
 {
+	@Autowired
+	private UserRepository repository;
 
+	@RequestMapping(method = RequestMethod.GET)
+	public Page<User> usersByType(final Pageable pageable,
+			@RequestParam(value = "filter", required = false, defaultValue = "ALL") final UserType type)
+	{
+		switch (type)
+		{
+		case ENABLED:
+			return repository.findByAccountDisabled(false, pageable);
+		case DISABLED:
+			return repository.findByAccountDisabled(true, pageable);
+		case LOCKED:
+			return repository.findByAccountLocked(true, pageable);
+		case NONLOCKED:
+			return repository.findByAccountLocked(false, pageable);
+		default:
+			return repository.findAll(pageable);
+		}
+	}
+
+	public static enum UserType
+	{
+		ALL,
+		ENABLED,
+		DISABLED,
+		LOCKED,
+		NONLOCKED,
+		EXPIRED,
+		NONEXPIRED,
+		CREDENTIALS_EXPIRED,
+		CREDENTIALS_NONEXPIRED
+	}
 }
