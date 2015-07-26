8ff7eca4e8f5598388576cc5a4866109e0570813
T-PWK <tompwk@gmail.com>
2015-07-26 16:07:17 +0100

    Search query updates

--- src/main/java/org/freezo/domain/UserRepository.java
+++ src/main/java/org/freezo/domain/UserRepository.java
@@ -14,41 +14,41 @@ public interface UserRepository extends PagingAndSortingRepository<User, Long>
 	/**
 	 * Finds a profile by it's account's login.
 	 * <p>
-	 * This method is used mainly by authentication mechanism and therefore it performs join fetching with account
-	 * entity.
+	 * This method is used mainly by authentication mechanism and therefore it
+	 * performs join fetching with account entity.
 	 *
-	 * @param username login
+	 * @param username
+	 *            login
 	 * @return optional profile entity
 	 */
 	@Query("select u from User u join fetch u.account where u.account.username = ?1")
 	Optional<User> findByUsername(String username);
 
-	@Query(value = "select u from User u join fetch u.account a where a.disabled = ?1",
-			countQuery = "select count(u) from User u where u.account.disabled = ?1")
+	@Override
+	@Query(value = "select u from User u inner join fetch u.account", countQuery = "select count(u) from User u")
+	Page<User> findAll(Pageable pageable);
+
+	@Query(value = "select u from User u inner join fetch u.account a where a.disabled = ?1",
+			countQuery = "select count(u) from User u inner join u.account where u.account.disabled = ?1")
 	Page<User> findByAccountDisabled(boolean disabled, Pageable pageable);
 
+	@Query(value = "select u from User u inner join fetch u.account a where a.locked = ?1",
+			countQuery = "select count(u) from User u inner join u.account where u.account.locked = ?1")
 	Page<User> findByAccountLocked(boolean locked, Pageable pageable);
 
-	@Query(value = "select u from User u join fetch u.account a where a.expired = true or a.accountExpirationDate < CURRENT_TIMESTAMP()",
-			countQuery = "select count(u) from User u where u.account.expired = true or u.account.accountExpirationDate < CURRENT_TIMESTAMP()")
+	@Query(value = "select u from User u join fetch u.account a where a.accountExpirationDate < current_timestamp()",
+			countQuery = "select count(u) from User u inner join u.account a where a.accountExpirationDate < current_timestamp()")
 	Page<User> findByAccountExpired(Pageable pageable);
 
-	@Query(value = "select u from User u join fetch u.account a where a.expired = false",
-			countQuery = "select count(u) from User u where u.account.expired = false")
+	@Query(value = "select u from User u join fetch u.account a where a.accountExpirationDate is null or a.accountExpirationDate > current_timestamp()",
+			countQuery = "select count(u) from User u inner join u.account a where a.accountExpirationDate is null or a.accountExpirationDate > current_timestamp()")
 	Page<User> findByAccountNonExpired(Pageable pageable);
 
-	// Hibernate: select count(user0_.id) as col_0_0_ from user user0_ left outer join account account1_ on
-	// user0_.id=account1_.user_id where account1_.disabled=?
-	// Hibernate: select user0_.id as id1_2_, user0_.bio as bio2_2_, user0_.email as email3_2_, user0_.first_name as
-	// first_na4_2_, user0_.last_modified as last_mod5_2_, user0_.last_name as last_nam6_2_, user0_.location as
-	// location7_2_, user0_.picture as picture8_2_, user0_.version as version9_2_, user0_.website as website10_2_ from
-	// user user0_ left outer join account account1_ on user0_.id=account1_.user_id where account1_.disabled=? limit ?
+	@Query(value = "select u from User u join fetch u.account a where a.credentialsExpirationDate < current_timestamp()",
+			countQuery = "select count(u) from User u inner join u.account a where a.credentialsExpirationDate < current_timestamp()")
+	Page<User> findByCredentialsExpired(Pageable pageable);
 
-	// Hibernate: select count(user0_.id) as col_0_0_ from user user0_ cross join account account1_ where
-	// user0_.id=account1_.user_id and account1_.disabled=?
-	// Hibernate: select user0_.id as id1_2_, user0_.bio as bio2_2_, user0_.email as email3_2_, user0_.first_name as
-	// first_na4_2_, user0_.last_modified as last_mod5_2_, user0_.last_name as last_nam6_2_, user0_.location as
-	// location7_2_, user0_.picture as picture8_2_, user0_.version as version9_2_, user0_.website as website10_2_ from
-	// user user0_ cross join account account1_ where user0_.id=account1_.user_id and account1_.disabled=? limit ?
-
+	@Query(value = "select u from User u join fetch u.account a where a.credentialsExpirationDate is null or a.credentialsExpirationDate > current_timestamp())",
+			countQuery = "select count(u) from User u inner join u.account a where a.credentialsExpirationDate is null or a.credentialsExpirationDate > current_timestamp())")
+	Page<User> findByCredentialsNonExpired(Pageable pageable);
 }
