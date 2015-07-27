919b5ac054cb8fec4cb7503b731c767c8f7a3278
T-PWK <tompwk@gmail.com>
2015-07-27 13:18:04 +0100

    Administration console template updates
    - New localisation values
    - AngularJS templates cleanup

--- src/main/resources/static/admin/js/application.js
+++ src/main/resources/static/admin/js/application.js
@@ -26,9 +26,10 @@
 				.otherwise('/dashboard');
 		}])
 		.factory('User', ['$resource', 'CsrfToken', function ($resource, CsrfToken) {
-			return $resource('/api/v1/users/:userId/:action', { userId: '@id', action: '@action' }, {
-				update: { method: 'PUT', headers: { 'X-CSRF-TOKEN': CsrfToken } }
-			});
+			return $resource('/api/v1/users/:userId', { userId: '@id' },
+				{
+					update: { method: 'PUT', headers: { 'X-CSRF-TOKEN': CsrfToken } }
+				});
 		}])
 		.filter('roleName', function () {
 			return function (role) {
@@ -43,7 +44,6 @@
 			}
 		})
 		.controller('DashboardCtrl', ['$scope', function ($scope) {
-			$scope.data = 'Hi';
 		}])
 		.controller('UsersCtrl', ['$scope', '$timeout', 'User', function ($scope, $timeout, User) {
 			$('.main .dropdown').dropdown();
@@ -53,7 +53,6 @@
 			$scope.asc = true;
 			$scope.pageNumber = 0;
 
-			$scope.query = { filter: null, sort: null, page: null };
 			$scope.filterBy = function (filter) {
 				$scope.filter = filter;
 				$scope.sort = null;
@@ -74,8 +73,8 @@
 			$scope.reload = function () {
 				User.get({
 					sort: $scope.sort ? ($scope.sort + ',' + ($scope.asc ? 'asc' : 'desc')) : null,
-					page: $scope.pageNumber,
-					filter: $scope.filter
+					page: $scope.pageNumber > 0 ? $scope.pageNumber : null,
+					filter: $scope.filter === 'all' ? null : $scope.filter
 				}, function (details) {
 					$scope.details = details;
 					$scope.users = details.content;
@@ -87,28 +86,33 @@
 			$scope.action = function (whatAction, user) {
 				$scope.whatAction = whatAction;
 				$scope.user = user;
-				$('.ui.modal.confirm').modal({
-					closable: false,
-					onApprove: function () {
-						User.update({ id: user.id, action: whatAction }, function () {
-							$scope.reload();
-						});
-					}
-				}).modal('show');
+				$timeout(function () {
+					$('.ui.modal.confirm').modal({
+						closable: false,
+						onApprove: function () {
+							User.update({ id: user.id, type: whatAction }, function () { $scope.reload(); });
+						}
+					}).modal('show');
+				});
 			}
 			$scope.edit = function (user) {
 				$scope.user = user;
-				$('.ui.modal.edit').modal('show');
+				$timeout(function () {
+					$('.ui.modal.edit').modal('show');
+				});
 			};
 			$scope.$on('$destroy', function () {
 				$('.ui.modal').remove();
 			});
-			$scope.isSortActive = function (sort) {
+			$scope.isSortBy = function (sort) {
 				return $scope.sort == sort;
 			};
-			$scope.isFilterActive = function (filter) {
-				return $scope.filter == filter || (filter == 'ALL' && !$scope.filter);
+			$scope.isFilterBy = function (filter) {
+				return $scope.filter == filter || (filter == 'all' && !$scope.filter);
 			};
+			$scope.filterCls = function (filter) {
+				return $scope.isFilterBy(filter) ? 'active' : null;
+			}
 			$scope.pagination = function () {
 				var pages = [], count = ($scope.details && $scope.details.totalPages) || 0;
 				for (var i = 2; i < count + 1; i++) {
