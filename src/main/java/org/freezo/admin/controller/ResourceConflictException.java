2036feeb960a219b70958a65a59f1525a3fb1d82
T-PWK <tompwk@gmail.com>
2015-07-27 13:19:40 +0100

    MVC resource conflict exception

---
+++ src/main/java/org/freezo/admin/controller/ResourceConflictException.java
@@ -0,0 +1,19 @@
+package org.freezo.admin.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class ResourceConflictException extends RuntimeException
+{
+	private static final long serialVersionUID = 1367415622541896581L;
+
+	public ResourceConflictException()
+	{
+	}
+
+	public ResourceConflictException(final String message)
+	{
+		super(message);
+	}
+}
