c3f686b96afbf805410ccc11970d5e67b0784285
T-PWK <tompwk@gmail.com>
2015-07-28 17:03:30 +0100

    Input validation exception

---
+++ src/main/java/org/freezo/admin/controller/InputValidationException.java
@@ -0,0 +1,20 @@
+package org.freezo.admin.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+public class InputValidationException extends RuntimeException
+{
+	private static final long serialVersionUID = -8643674983268381338L;
+
+	public InputValidationException()
+	{
+	}
+
+	public InputValidationException(final String message)
+	{
+		super(message);
+	}
+
+}
