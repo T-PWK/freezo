9ca9d1cb0bf151ca9fe6cac837ff05f7b325a1c3
T-PWK <tompwk@gmail.com>
2015-08-24 12:15:45 +0100

    Home controller updates

--- src/main/java/org/freezo/web/controller/HomeController.java
+++ src/main/java/org/freezo/web/controller/HomeController.java
@@ -1,17 +1,35 @@
 package org.freezo.web.controller;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @Profile("web")
+@Controller
 public class HomeController
 {
 	@RequestMapping("/")
 	public String home()
 	{
-		return "Welcome to Freezo";
+		return "index";
+	}
+
+	@RequestMapping("/search/label/{label}")
+	public String label()
+	{
+		return "index";
+	}
+
+	@RequestMapping("/page/{page}")
+	public String pagination()
+	{
+		return "index";
+	}
+
+	@RequestMapping("/{slug}")
+	public String page()
+	{
+		return "index";
 	}
 
 }
