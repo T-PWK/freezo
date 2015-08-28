6a93b5572791647a19e1716c74090b6847478d01
T-PWK <tompwk@gmail.com>
2015-08-28 22:39:12 +0100

    Websites API changes

--- src/main/java/org/freezo/admin/controller/api/WebsitesController.java
+++ src/main/java/org/freezo/admin/controller/api/WebsitesController.java
@@ -2,8 +2,15 @@ package org.freezo.admin.controller.api;
 
 import java.util.Date;
 
-import org.freezo.domain.Website;
-import org.freezo.domain.WebsiteRepository;
+import javax.validation.Valid;
+
+import org.freezo.admin.controller.ResourceConflictException;
+import org.freezo.admin.domain.WebsiteForm;
+import org.freezo.admin.service.ModelMapper;
+import org.freezo.core.domain.Website;
+import org.freezo.core.domain.Website.Status;
+import org.freezo.core.domain.WebsiteRepository;
+import org.freezo.core.web.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +18,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Profile("admin")
 @RestController
-@RequestMapping("/api/v1/websites")
+@RequestMapping("${freezo.admin.urls.api}/websites")
 @Secured("ROLE_ADMIN")
 public class WebsitesController
 {
@@ -28,32 +37,135 @@ public class WebsitesController
 	@Autowired
 	private WebsiteRepository repository;
 
+	@Autowired
+	private ModelMapper mapper;
+
 	@RequestMapping(method = RequestMethod.GET)
+	@Transactional(readOnly = true)
 	public Page<Website> websites(final Pageable pageable) throws InterruptedException
 	{
+		LOG.debug("Websites listing: {}", pageable);
+
 		return repository.findAll(pageable);
 	}
 
 	@RequestMapping(value = "{id}", method = RequestMethod.GET)
-	public Website website(@PathVariable("id") final long id) throws InterruptedException
+	@Transactional(readOnly = true)
+	public Website website(@PathVariable("id") final long id)
 	{
-		return repository.findOne(id);
+		LOG.debug("Requesting website:[{}] details", id);
+
+		return findWebsite(id);
 	}
 
 	@RequestMapping(value = "{id}", method = RequestMethod.POST)
-	public Website update(@RequestBody final Website website, @PathVariable("id") final long id)
+	@Transactional
+	public Website update(@PathVariable("id") final long id,
+			@RequestBody @Valid final WebsiteForm form, final BindingResult result)
 	{
-		LOG.debug("Updating website:[{}] with content: {}", id, website);
+		LOG.debug("Updating website:[{}] with content: {} (errors:{})", id, form, result.hasErrors());
 
+		final Website website = findWebsite(id);
+
+		mapper.update(form, website);
 		website.setUpdatedAt(new Date());
+
 		return repository.save(website);
 	}
 
+	@RequestMapping(value = "{id}/disable", method = RequestMethod.PATCH)
+	@Transactional
+	public Website disable(@PathVariable("id") final long id)
+	{
+		LOG.debug("Disabling website:[{}]", id);
+
+		final Website website = findWebsite(id);
+
+		if (website.getStatus() != Status.ENABLED)
+		{
+			throw new ResourceConflictException("Invalid status");
+		}
+
+		website.setUpdatedAt(new Date());
+		website.setStatus(Status.DISABLED);
+
+		repository.save(website);
+
+		return website;
+	}
+
+	@RequestMapping(value = "{id}/enable", method = RequestMethod.PATCH)
+	@Transactional
+	public Website enable(@PathVariable("id") final long id)
+	{
+		LOG.debug("Enabling website:[{}]", id);
+
+		final Website website = findWebsite(id);
+
+		if (website.getStatus() != Status.DISABLED)
+		{
+			throw new ResourceConflictException("Invalid status");
+		}
+
+		website.setUpdatedAt(new Date());
+		website.setStatus(Status.ENABLED);
+
+		repository.save(website);
+
+		return website;
+	}
+
+	@RequestMapping(value = "{id}/restore", method = RequestMethod.PATCH)
+	public Website restore(@PathVariable("id") final long id)
+	{
+		LOG.debug("Restoring website:[{}]", id);
+
+		final Website website = findWebsite(id);
+
+		if (website.getStatus() != Status.PENDING_REMOVE)
+		{
+			throw new ResourceConflictException("Invalid status");
+		}
+
+		website.setUpdatedAt(new Date());
+		website.setStatus(Status.DISABLED);
+
+		repository.save(website);
+
+		return website;
+	}
+
 	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	@Transactional
 	public void delete(@PathVariable("id") final long id)
 	{
 		LOG.debug("Deleting website:[{}]", id);
 
-		repository.delete(id);
+		final Website website = findWebsite(id);
+
+		website.setUpdatedAt(new Date());
+		website.setDeleteRequestedAt(new Date());
+		website.setStatus(Status.PENDING_REMOVE);
+
+		repository.save(website);
+	}
+
+	/**
+	 * Checks if there is a website with the given identifier and returns found instance.
+	 *
+	 * @param id website identifier
+	 * @throws ResourceNotFoundException if there is no website with the given identifier.
+	 * @return website entity
+	 */
+	private Website findWebsite(final long id) throws ResourceNotFoundException
+	{
+		if (!repository.exists(id))
+		{
+			throw new ResourceNotFoundException(String.format("Website ID:%s does not exist", id));
+		}
+
+		final Website website = repository.findOne(id);
+
+		return website;
 	}
 }
