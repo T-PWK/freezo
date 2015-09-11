cdd379283efaa7c4d3e9ee9e5ea8245a907a75ae
T-PWK <tompwk@gmail.com>
2015-09-11 11:13:24 +0100

    New lookup query
    Find websites by status

--- freezo-core/src/main/java/org/freezo/domain/WebsiteRepository.java
+++ freezo-core/src/main/java/org/freezo/domain/WebsiteRepository.java
@@ -3,12 +3,16 @@ package org.freezo.domain;
 import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WebsiteRepository extends PagingAndSortingRepository<Website, Long>
 {
+	Page<Website> findByStatus(final Website.Status status, Pageable pageable);
+
 	Optional<Website> findByHostsAndStatus(String host, Website.Status status);
 
 	Set<Website> findByHostsIn(Set<String> hosts);
